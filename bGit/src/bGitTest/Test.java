f70ad44af95dfa3d88cdf8ad818c52b81234619d
USER <USER@DESKTOP-IVU51AD>
2020-05-18 17:07:58 +0900

    나조원2담

--- bGit/src/bGitTest/Test.java
+++ bGit/src/bGitTest/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("¾È´¨?");
 		System.out.println("¾È´¨ ¹Ý°¡¿ö ³­ Á¶¿øÀÌ¾ä");
+		System.out.println("YYYYYYYYYYYY");
 	}
 
 }
